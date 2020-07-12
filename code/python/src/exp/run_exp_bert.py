95b621d3a0a1f82ec1eb2a92410d29a69f31e3a9
zz <ziqizhang.email@gmail.com>
2020-07-12 11:00:39 +0100

    .

--- code/python/src/exp/run_exp_bert.py
+++ code/python/src/exp/run_exp_bert.py
@@ -70,11 +70,20 @@ if __name__ == "__main__":
 
     properties = exp_util.load_properties(setting_file)
 
-    df_all, train_size, test_size = exp_util.load_and_merge_train_test_data_jsonMPWD(
-        # "/home/zz/Cloud/GDrive/ziqizhang/project/mwpd/prodcls/data/swc2020/small_train.json",
-        # "/home/zz/Cloud/GDrive/ziqizhang/project/mwpd/prodcls/data/swc2020/small_train.json")
-        train,
-        test)
+    print("loading dataset...")
+    if sys.argv[5] == "mwpd":
+        dfdf_all, train_size, test_size = exp_util. \
+            load_and_merge_train_test_data_jsonMPWD(train, test)
+    elif sys.argv[5] == "rakuten":
+        dfdf_all, train_size, test_size = exp_util. \
+            load_and_merge_train_test_csvRakuten(train, test, delimiter="\t")
+    elif sys.argv[5] == "icecat":
+        dfdf_all, train_size, test_size = exp_util. \
+            load_and_merge_train_test_data_jsonIceCAT(train, test)
+    else:  # wdc
+        dfdf_all, train_size, test_size = exp_util. \
+            load_and_merge_train_test_data_jsonWDC(train, test)
+
 
     class_fieldname = exp_util.load_setting("class_fieldname", properties, overwrite_params)
     param_label_field = text_field_mapping[class_fieldname]
